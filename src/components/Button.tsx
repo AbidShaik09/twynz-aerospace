a104e6f5415113049d2814567262b939708847cb
Shaik Abid Hussain <hussainabidshaik@gmail.com>
2025-08-31 09:29:12 -0700

    Adding Buttons to section 1

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+const Button = (prop: {
+  onClick: () => void;
+  label: string;
+  type?: string;
+}) => {
+  const { type = "primary", label, onClick } = prop;
+  return (
+    <button
+      className={`${type} text-xs`}
+      onClick={onClick}
+      style={{ border: "4px" }}
+    >
+      {label}
+    </button>
+  );
+};
+export default Button;
