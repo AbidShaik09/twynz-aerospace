058195f569cf45ae1614af129004fc9b042b7d74
Shaik Abid Hussain <hussainabidshaik@gmail.com>
2025-08-31 10:14:21 -0700

    Moving Home Page Components to a common directory

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer";
-import Section1 from "../../components/Section1";
-import Section2 from "../../components/Section2";
+import Section1 from "../../components/homePage/Section1";
+import Section2 from "../../components/homePage/Section2";
+import Section3 from "../../components/homePage/Section3";
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <Section1 />
       <Section2 />
+      <Section3 />
       <Footer />
     </div>
   );
