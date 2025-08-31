67d004e1865962ff81b2350c2bea6e34c516f8e6
ajay-30 <20r21a05c6@mlrinstitutions.ac.in>
2025-08-31 17:54:33 +0530

    added section-2 and footer to the home page

---
+++ src/components/Section2.tsx
@@ -0,0 +1,20 @@
+const Section2 = () => {
+  return (
+    <div className="mb-5">
+      <div className="ml-5">
+        <h4 className="h4-accent">What we do</h4>
+        <h1>Let's Touch Base!</h1>
+      </div>
+
+      <div className="w-full lg:w-2/5">
+        <img
+          src="/images/ai-icon-01.png"
+          alt="AI Icon"
+          className="w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
