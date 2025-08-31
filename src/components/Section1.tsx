498d18819aed279b51af71b5a2a4114a1237ad99
Shaik Abid Hussain <hussainabidshaik@gmail.com>
2025-08-31 04:56:04 -0700

    added some css

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -1,8 +1,27 @@
 const Section1 = () => {
   return (
-    <div className="flex mt-5 flex-col-reverse lg:flex-row">
-      <div className="w-full lg:w-3/5">hi</div>
-      <div className="w-full lg:w-2/5">
+    <div className="flex flex-col-reverse sm:flex-row">
+      <div className="w-full sm:w-4/10 py-10 pl-10 flex justify-center items-center">
+        <div className="text-content">
+          <div className="text-xl text-secondary">
+            Sustainable. Scalable. Transformative.
+          </div>
+
+          <div className="font-bold text-4xl ">
+            Engineering the Future of Flight
+          </div>
+
+          <div>
+            At Twynz Aerospace Ltd., we blend engineering excellence,
+            sustainable technology, and startup agility to create meaningful
+            transportation solutions for a better-connected future.
+            Headquartered in London, UK, we focus on solving short-distance
+            travel inefficiencies while building a platform for advanced
+            engineering consultancy.
+          </div>
+        </div>
+      </div>
+      <div className="w-full sm:w-6/10">
         <img
           src="/images/airplane.png"
           alt="Airplane"
